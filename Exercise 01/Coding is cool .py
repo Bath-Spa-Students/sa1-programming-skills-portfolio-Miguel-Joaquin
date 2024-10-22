c558e23fff2dccb02c2cb8f1e0f9c510f481417f
Miguel-Joaquin <miguel.bsu07@gmail.com>
2024-10-22 14:08:53 +0400

    Update Coding is cool .py

--- Exercise 01/Coding is cool .py
+++ Exercise 01/Coding is cool .py
@@ -1,3 +1,5 @@
+# Fill in the blanks in the Python code below to output the phrase "Coding is Cool" to the console using variables and string concatenation.
+
 word1 = "Coding"
 word2 = "Is"
 word3 = "Cool"
