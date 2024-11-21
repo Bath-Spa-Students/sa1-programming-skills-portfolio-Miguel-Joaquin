03fdb83569b0d7756951aa6e3bd0300027413af5
Miguel-Joaquin <miguel.bsu07@gmail.com>
2024-11-21 21:58:38 +0400

    Biography.py

---
+++ Exercise 03 /Biography.py
@@ -0,0 +1,8 @@
+# Biography
+
+Biography = { 'Name' : 'Miguel Joaquin' , 
+              'Hometown': 'Dubai, UAE' ,
+              'Age' : '22' } 
+
+for key, value in Biography.items():
+    print(key, ' : ' ,value)
