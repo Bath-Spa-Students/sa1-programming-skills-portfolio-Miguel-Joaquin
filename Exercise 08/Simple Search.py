03834b7ae818ba2e6349ce66976e94af82a99730
Miguel-Joaquin <miguel.bsu07@gmail.com>
2024-11-21 23:31:31 +0400

    Simple Search.py

---
+++ Exercise 08/Simple Search.py
@@ -0,0 +1,16 @@
+# Simple Search
+
+# The list of the Names:
+
+Names = ("Jake", "Zac", "Ian", "Ron", "Sam", "Dave")
+
+# Find the name for
+Name_Finder = "Sam"
+
+# Search the name in the list
+
+if Name_Finder in Names:
+    print(f"{Name_Finder} is in the list")
+
+else:
+    print(f"{Name_Finder} Not found in the list")
