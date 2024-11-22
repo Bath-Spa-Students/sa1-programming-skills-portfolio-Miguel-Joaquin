f5bd58f089d0078dd333a8dbb74cf4759e130e79
Miguel-Joaquin <miguel.bsu07@gmail.com>
2024-11-22 10:58:36 +0400

    Primitive Quiz.py

---
+++ Exercise 04/Primitive Quiz.py
@@ -0,0 +1,7 @@
+# To ask the question and check the answer
+
+Capital = str(input("What is the capital of France? "))
+if Capital.lower() == "paris":
+    print("The answer is Correct")
+else:
+    print("The answer is Wrong")
