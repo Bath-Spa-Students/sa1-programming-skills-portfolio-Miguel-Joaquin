d9ad892e5600f9f19bf6e4becaf3d887d933b805
Miguel-Joaquin <miguel.bsu07@gmail.com>
2024-11-22 19:11:09 +0400

    String Concatentation.py

---
+++ z_Bonus_Exercises/String Concatentation.py
@@ -0,0 +1,9 @@
+# Strings Concatenation:
+
+# Write three strings in different variables and print the output as one string.
+
+str1 = "Bonus"
+str2 = " "
+str3 = "Exercises"
+
+print (str1 + str2 + str3)
