e82821e2ba831fc873aa37443428bcb412495889
Miguel-Joaquin <miguel.bsu07@gmail.com>
2024-11-22 19:14:53 +0400

    Control Structures.py

---
+++ z_Bonus_Exercises/Control Structures.py
@@ -0,0 +1,10 @@
+# Control Structures:
+
+alien_color = "red"
+
+if alien_color == "red":
+    print("Congratulation, You Earned 5 Points")
+
+alien_color = 'green'
+if alien_color != "red":
+    print("You Failed")
