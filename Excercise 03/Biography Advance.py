cb181756c81774c05fa39442796d1f565c560980
Miguel-Joaquin <miguel.bsu07@gmail.com>
2024-11-22 10:30:59 +0400

    Delete Excercise 03 directory

--- Excercise 03/Biography Advance.py
+++
@@ -1,7 +0,0 @@
-# Biography Advance
-
-Biography = {'Name': str(input("Name: ")),
-             'Address':  str(input("Address: ")),
-              'Age': str(input("Age: "))}
-
-print("\nName:" + " " + Biography["Name"], "\nAge:" + " " + Biography["Age"], "\nAddress:" + " " + Biography["Address"])
