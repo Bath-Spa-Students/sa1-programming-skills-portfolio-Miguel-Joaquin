5992d8265a6836daf5bd932a5c77306bf8190dfb
Miguel-Joaquin <miguel.bsu07@gmail.com>
2024-11-22 10:27:38 +0400

    Biography Advance.py

---
+++ Exercise 03 /Biography Advance.py
@@ -0,0 +1,7 @@
+# Biography Advance
+
+Biography = {'Name': str(input("Name: ")),
+             'Address':  str(input("Address: ")),
+              'Age': str(input("Age: "))}
+
+print("\nName:" + " " + Biography["Name"], "\nAge:" + " " + Biography["Age"], "\nAddress:" + " " + Biography["Address"])
