00826909b389c2103cb1c2ee797e607bd2e94417
Miguel-Joaquin <miguel.bsu07@gmail.com>
2024-11-22 19:13:23 +0400

    Variables Favorite Number.py

---
+++ z_Bonus_Exercises/Variables Favorite Number.py
@@ -0,0 +1,10 @@
+# Variables (Favorite Number)
+
+# Use a variable to represent your favorite number. Then, using that variable, create a
+# message that reveals your favorite number. Print that message.
+
+Favorite_Number = 3
+
+Message = f"My favorite number is {Favorite_Number}"
+
+print(Message)
