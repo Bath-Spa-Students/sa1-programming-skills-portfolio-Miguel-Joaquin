5fd1711598a2b721d7ea6468108b27431bf8e376
Miguel-Joaquin <miguel.bsu07@gmail.com>
2024-11-22 12:07:40 +0400

    Some Counting.py

---
+++ Exercise 07/Some Counting.py
@@ -0,0 +1,32 @@
+# 1) Loop that counts up from 0 to 50 in increments of 1
+print ("Counting up from 0 to 50 in increments of 1:")
+for M in range(0, 51,1):  # Start at 0, stop at 50, step by 1
+    print(M)
+
+print ("\n")
+
+# 2) Loop that counts down from 50 to 0 in decrements of 1
+print("Counting down from 50 to 0 in decrements of 1:")
+for M in range(50, -1,-1):  # Start at 50, stop at 0, step by -1
+    print(M)
+
+print ("\n")
+
+# 3) Loop that counts up from 30 to 50 in increments of 1
+print("Counting up from 30 to 50 in increments of 1:")
+for M in range(30, 51,1):  # Start at 30, stop at 50, step by 1
+    print(M)
+
+print ("\n")
+
+# 4) Loop that counts down from 50 to 10 in decrements of 2
+print("Counting down from 50 to 10 in decrements of 2:")
+for M in range(50, 9, -2):  # Start at 50, stop at 10 (9 because range is exclusive at the stop), step by -2
+    print(M)
+
+print ("\n")
+
+# 5) Loop that counts up from 100 to 200 in increments of 5
+print("Counting up from 100 to 200 in increments of 5:")
+for M in range(100, 201, 5):  # Start at 100, stop at 200, step by 5
+    print(M)
